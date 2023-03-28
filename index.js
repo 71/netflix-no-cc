a1a315d1428b811608dc6c7e200ac5b8ea6fd327
Grégoire Geis <opensource@gregoirege.is>
2023-03-28 20:18:23 +0200

    fix for Firefox, better code style, violentmonkey compatibility

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-let lastContainer = null
+// ==UserScript==
+// @name        Netflix — No closed captions
+// @match       https://www.netflix.com/watch/*
+// @grant       none
+// ==/UserScript==
+
+// Disable the condition below if the script is not running in ViolentMonkey.
+if (true) {
+  document.body.appendChild(
+    Object.assign(
+      document.createElement("style"), {
+        innerHTML: `
+          .player-timedtext {
+            display: flex!important;
+            justify-content: center;
+          }
+
+          .player-timedtext-text-container {
+            left: auto!important;
+          }
+        `,
+      },
+    ),
+  );
+}
+
+let lastContainer = null;
 
 function removeClosedCaptions() {
-  const els = document.getElementsByClassName('player-timedtext-text-container')
+  const els = document.getElementsByClassName("player-timedtext-text-container");
 
   if (els.length === 1) {
-    const el = els[0]
+    const el = els[0];
 
-    if (el != lastContainer && el.children != null) {
-      lastContainer = el
+    if (el !== lastContainer && el.children !== null) {
+      lastContainer = el;
 
-      for (const e of el.children) {
-        for (const n of e.childNodes) {
-          if (n.nodeValue)
-            n.nodeValue = n.nodeValue.replace(/ *-? *\[.+?\] */g, '')
+      const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT);
+
+      for (;;) {
+        const node = walker.nextNode();
+
+        if (node === null) {
+          break;
         }
+
+        node.nodeValue = node.nodeValue.replace(/ *-? *\[.+?\] */g, "");
       }
     }
   }
 
-  requestAnimationFrame(removeClosedCaptions)
+  requestAnimationFrame(removeClosedCaptions);
 }
 
-removeClosedCaptions()
+removeClosedCaptions();
